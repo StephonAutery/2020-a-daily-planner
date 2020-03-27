76eee92df7ce9ee8aba180b6bf57606c29d0e327
StephonAutery <stephon@stephonautery.com>
2020-03-27 09:56:42 -0700

    rediculous contraption

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,56 +8,77 @@ $(".jumbotron").append(timeDiv);
 var amTxt = "am";
 var pmTxt = "pm";
 var eventState = "";
+var hoursA = "";
+var hoursP = "";
+var uId = "";
 
 for (let i = 0; i < 9; i++) {
     var rowDiv = $('<div id="row" class="row"></div>');
     var momentHour = moment().format("h");
     var amPm = moment().format("a");
     $(".container").append(rowDiv);
-    var hoursA = i + 9;
-    var hoursP = i - 3;
+    hoursA = i + 9;
+    hoursP = i - 3;
 
     function hours() {
         if (i <= 3 && hoursA === momentHour) {
             // console.log("Apresent - hoursA : " + hoursA);
             eventState = "present";
-            return hoursA + amTxt;
+            uId = hoursA + amTxt;
+            return uId;
         } else if (i <= 3 && momentHour < hoursA && amPm === "am") {
             // console.log("Afuture - hoursA : " + hoursA);
             eventState = "future";
-            return hoursA + amTxt;
+            uId = hoursA + amTxt;
+            return uId;
         }
         else if (i <= 3 && amPm === "pm") {
             // console.log(" APast - hoursA : " + hoursA);
             eventState = "past";
-            return hoursA + amTxt;
+            uId = hoursA + amTxt;
+            return uId;
         } else if (hoursP === momentHour) {
             // console.log(" hoursA : " + hoursA);
             eventState = "present";
-            return hoursA + amTxt;
+            uId = hoursP + pmTxt
+            return uId;
         } else if (hoursP > momentHour) {
             // console.log(" hoursP : " + hoursP);
             eventState = "future";
-            return hoursP + pmTxt;
+            uId = hoursP + pmTxt
+            return uId;
         }
         else if (hoursP < momentHour) {
             // console.log(" hoursP : " + hoursP);
             eventState = "past";
-            return hoursP + pmTxt;
+            uId = hoursP + pmTxt
+            return uId;
         }
     }
-
     // console.log(" ---> " + eventState);
     // console.log(hours());
-    var hourDiv = $('<div id="hour"></div>').text(hours());
-    hourDiv.attr("class", "hour col-3 ");
+    var hourDiv = $('<div id="hour'+ hours() +'" data-hour="'+ hours() +'"></div>').text(hours());
+    hourDiv.attr("class", "hour col-3");
     rowDiv.append(hourDiv);
-    var eventDiv = $('<input type="textarea" id="event"></div>');
-    eventDiv.attr("class", "time-block col-6 " + eventState);
+
+    var eventDiv = $('<input id="newEvent'+ hours() +'" type="text" data-hour="'+ hours() +'"></div>');
+    eventDiv.attr("class", "time-block col-6 "+ eventState);
     rowDiv.append(eventDiv);
-    var saveDiv = $('<button id="save"></button>').text("save");
-    saveDiv.attr("class", "saveBtn col-3");
+    
+    var saveDiv = $('<button id="save'+ hours() +'"data-hour="'+ hours() +'"></button>').text("save");
+    saveDiv.attr("class", "saveBtn col-3 "+ eventState);
+    // saveDiv.attr("class", eventState);
     rowDiv.append(saveDiv);
 }
 
+function getBtn() {
+    // 
+    // event.preventDefault();
+    console.log(this);
+    console.log(this.id);
+    console.log($(this).attr("data-hour"));
+}
+
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+
+$(document).on("click", ".saveBtn", getBtn);
---
+++ javascript/sla-data.js
@@ -0,0 +1,5 @@
+{
+    eventDate: 
+    eventTime:
+    eventTitle:
+}
